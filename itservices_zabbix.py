c22ad69ccd9fb81d2fe6339fd0a0ea67356b2236
Sourcery AI <>
2023-09-08 00:20:52 +0000

    'Refactored by Sourcery'

--- itservices_zabbix.py
+++ itservices_zabbix.py
@@ -24,8 +24,9 @@ def get_hostgroups():
     return listaGrupos
 
 def get_hostgroups_id(grupo):
-    groupId = zapi.hostgroup.get({"output": "extend","filter":{"name":grupo}})[0]['groupid']
-    return groupId
+    return zapi.hostgroup.get({"output": "extend", "filter": {"name": grupo}})[
+        0
+    ]['groupid']
 
 def get_hosts(grupo):
     hosts_grupo = zapi.host.get({"groupids":get_hostgroups_id(grupo),"output":["host"]})
@@ -36,8 +37,7 @@ def get_hosts(grupo):
     return listaHosts
 
 def get_hostid(host):
-    hostId = zapi.host.get({"output":"hostid","filter":{"host":host}})[0]['hostid']
-    return hostId
+    return zapi.host.get({"output":"hostid","filter":{"host":host}})[0]['hostid']
 
 def get_triggers_hosts(host):
     triggers = zapi.trigger.get({"hostids":get_hostid(host),"expandDescription":"true","expandComment":"true","expandExpression":"true"})
@@ -53,32 +53,48 @@ def get_items_hosts(host):
     return listaItems
 
 def get_item_triggerid(host,item):
-    triggerId = zapi.item.get({"output":"triggers","hostids":get_hostid(host),"with_triggers":True,"selectTriggers":"triggers","filter":{"name":item}})[0]['triggers'][0]['triggerid']
-    return triggerId
+    return zapi.item.get(
+        {
+            "output": "triggers",
+            "hostids": get_hostid(host),
+            "with_triggers": True,
+            "selectTriggers": "triggers",
+            "filter": {"name": item},
+        }
+    )[0]['triggers'][0]['triggerid']
 
 def mk_father_itservices(grupo):
     zapi.service.create({"name":grupo,"algorithm":"1","showsla":"1","goodsla":"99.99","sortorder":"1"})
 
 def get_itservice_pid(grupo):
-    parentId = zapi.service.get({"selectParent":"extend","selectTrigger":"extend","expandExpression":"true","filter":{"name":grupo}})[0]['serviceid']
-    return parentId
+    return zapi.service.get(
+        {
+            "selectParent": "extend",
+            "selectTrigger": "extend",
+            "expandExpression": "true",
+            "filter": {"name": grupo},
+        }
+    )[0]['serviceid']
 
 def mk_child_itservices(host,grupo):
     zapi.service.create({"name":host,"algorithm":"1","showsla":"1","goodsla":"99.99","sortorder":"1","parentid":get_itservice_pid(grupo)})
 
 def get_itservice_pid_child(host):
-    parentIdChild = zapi.service.get({"selectParent":"extend","selectTrigger":"extend","expandExpression":"true","filter":{"name":host}})[0]['serviceid']
-    return parentIdChild
+    return zapi.service.get(
+        {
+            "selectParent": "extend",
+            "selectTrigger": "extend",
+            "expandExpression": "true",
+            "filter": {"name": host},
+        }
+    )[0]['serviceid']
 
 def mk_child_itservices_trigger(host,item):
     zapi.service.create({"name":item,"algorithm":"1","showsla":"1","goodsla":"99.99","sortorder":"1","parentid":get_itservice_pid_child(host),"triggerid":get_item_triggerid(host,item)})
 
 def get_itservices():
     itServices = zapi.service.get({"selectParent":"extend","selectTrigger":"extend"})
-    listaServicos = []
-    for x in itServices:
-        listaServicos += [x['serviceid']]
-    return listaServicos
+    return [x['serviceid'] for x in itServices]
 
 def delete_tree_itservices():
     for x in get_itservices():
